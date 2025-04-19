99ee77b41d8702446da0c533a6c8cd2f0f1ee779
Jon Arve Ovesen <jonovesen@gmail.com>
2025-04-19 22:52:49 +0200

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -1,1 +1,13 @@
+from sklearn.neural_network import MLPClassifier
+import numpy as np
 
+data = np.array([[8, 1], [12, 0], [15, 1]])
+labels = np.array([1, 0, 1])
+
+model = MLPClassifier(hidden_layer_sizes=(10, 10), max_iter=1000)
+model.fit(data, labels)
+
+def predict_smart_access(time, history):
+    return model.predict([[time, history]])[0]
+
+print(predict_smart_access(10, 1))  # Dynamisk adgangsvurdering!
