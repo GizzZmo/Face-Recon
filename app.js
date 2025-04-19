593a30b4e246212cf34cb7ffb9a751f021d1ea0c
Jon Arve Ovesen <jonovesen@gmail.com>
2025-04-19 23:23:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+async function requestAccess() {
+    const response = await fetch('/access', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ user: "Jon" })
+    });
+    const data = await response.json();
+    alert(data.access ? "Adgang tillatt!" : "Ingen adgang!");
+}
